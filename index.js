cae722dd7c9f863811e6bcc583e9c62230fd3e91
Kim Carter <Kim.Carter@binarymist.net>
2018-05-31 18:52:09 +1200

    Added npm scripts Linted

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-const Joi - require('joi');
+const Joi = require('joi');
 
 const loggerSchema = {
   level: Joi.string().valid(['emerg', 'alert', 'crit', 'error', 'warning', 'notice', 'info', 'debug'])
@@ -8,40 +8,34 @@ const loggerSchema = {
 let properties;
 let loggerInstance;
 
-const validateOptions = loggerOptions => {
+const validateOptions = (loggerOptions) => {
   const result = Joi.validate(loggerOptions, loggerSchema);
-  if(result.error) {
-    console.log(result.error.message);
-    throw new Error(result.error.message);
-  }
+  if (result.error) throw new Error(result.error.message);
   return result.value;
 };
 
 
 const logger = () => {
-  if(!loggerInstance) throw new Error('You must call init(options) first.');
+  if (!loggerInstance) throw new Error('You must call init(options) first.');
   return loggerInstance;
 };
 
 
 const createLogger = () => {
-     
   loggerInstance = winston.createLogger({
-
     levels: winston.config.syslog,
     level: properties.level,
     transports: [
       new winston.transports.Console()
-    ],
-      
+    ]
   });
 
   return loggerInstance;
 };
 
 
-const init = options => {
-  if(loggerInstance) return loggerInstance;
+const init = (options) => {
+  if (loggerInstance) return loggerInstance;
   properties = validateOptions(options);
   return createLogger();
 };
